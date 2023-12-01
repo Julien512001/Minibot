b21c1a33fdee6b39938e3bfe658374107762d059
Julien512001 <julien.moulis@student.uclouvain.be>
2023-12-01 17:42:14 +0100

    FIX

--- lidar_example_cpp-main/main_folder/main.cpp
+++ lidar_example_cpp-main/main_folder/main.cpp
@@ -13,14 +13,11 @@ int main(int argc, const char * argv[]){
   ILidarDriver *lidar;
 	lidar = connectLidar();
 
-
-<<<<<<< HEAD
   sl_lidar_response_measurement_node_hq_t local_buf[256];
   size_t count;
 
   //lidar->startScan(1,1,0,nullptr);
   lidar->grabScanDataHq(local_buf, count);
-=======
   sl_lidar_response_measurement_node_hq_t nodes[8192];
   size_t nodeCount = sizeof(nodes)/sizeof(sl_lidar_response_measurement_node_hq_t);
   res = lidar->grabScanDataHq(nodes, nodeCount);
@@ -31,13 +28,10 @@ int main(int argc, const char * argv[]){
   float angle_in_degrees   = nodes->angle_z_q14 * 90.f / (1 << 14);
   float distance_in_meters = nodes->dist_mm_q2 / 1000.f / (1 << 2);
 
->>>>>>> 636b62ed8d100780bb8557184694490a32211be9
 
-  
 
-  for (int i = 0; i < 0; i++) {
-    printf("%lf\n", (double) local_buf[i]);
-  }
+  printf("%f\n", angle_in_degrees);
+  printf("%f\n", distance_in_meters);
 
 	disconnectLidar(lidar);
 
