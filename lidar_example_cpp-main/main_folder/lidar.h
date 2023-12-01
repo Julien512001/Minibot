636b62ed8d100780bb8557184694490a32211be9
Julien Moulis <63585793+Julien512001@users.noreply.github.com>
2023-12-01 10:38:35 +0100

    Lidar

--- lidar_example_cpp-main/main_folder/lidar.h
+++ lidar_example_cpp-main/main_folder/lidar.h
@@ -4,6 +4,9 @@
 
 #include "sl_lidar.h"
 #include "sl_lidar_driver.h"
+#include "rplidar.h"
+#include "rplidar_driver.h"
+
 
 
 
--- lidar_example_cpp-main/main_folder/main.cpp
+++ lidar_example_cpp-main/main_folder/main.cpp
@@ -9,8 +9,20 @@ using namespace sl;
 
 int main(int argc, const char * argv[]){
 
+  int res;
   ILidarDriver *lidar;
 	lidar = connectLidar();
+
+
+  sl_lidar_response_measurement_node_hq_t nodes[8192];
+  size_t nodeCount = sizeof(nodes)/sizeof(sl_lidar_response_measurement_node_hq_t);
+  res = lidar->grabScanDataHq(nodes, nodeCount);
+  if (res != 0) {
+    fprintf(stderr, "Failed to get data");
+  }
+
+  float angle_in_degrees   = nodes->angle_z_q14 * 90.f / (1 << 14);
+  float distance_in_meters = nodes->dist_mm_q2 / 1000.f / (1 << 2);
 
 
   
